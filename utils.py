54ae9ad6f2b605f6107d4662dcf251a5c3a9b922
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-08-29 13:21:48 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,17 +1,17 @@
-import requests
+import pdftables_api
 import tempfile
 
-def convert_pdf_to_excel(pdf_file, supplier):
-    temp_excel_path = tempfile.mktemp(suffix='.xlsx')  # Créer un fichier temporaire
-    
-    # Convertir le fichier PDF en Excel avec l'API PDFTables
-    url = f"https://pdftables.com/api?key=o6xspb5x8fq4&format=xlsx-single"
-    files = {'file': pdf_file}
-    response = requests.post(url, files=files, stream=True)
-    
-    # Sauvegarder le fichier converti
-    with open(temp_excel_path, 'wb') as f:
-        f.write(response.content)
-    
+def convert_pdf_to_excel(pdf_path):
+    # Créez un fichier temporaire pour stocker le fichier Excel converti
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.xlsx') as temp_file:
+        temp_excel_path = temp_file.name
+
+    # Initialiser le client API avec la clé API
+    client = pdftables_api.Client(o6xspb5x8fq4)
+
+    # Convertir le PDF en Excel avec un seul onglet
+    client.xlsx-single(pdf_path, temp_excel_path)
+
     print(f"Conversion réussie, fichier sauvegardé sous : {temp_excel_path}")
+
     return temp_excel_path
