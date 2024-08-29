8c13eadaa9652dcf2d1030b1c4b34af9e81b797a
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-08-29 13:13:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import tempfile
+import os
 from utils import convert_pdf_to_excel
 from converters.yes_food_converter import process_yes_food
 from converters.mtc_converter import process_mtc
@@ -24,17 +25,33 @@ def main():
             # Bouton de confirmation
             if st.button("Confirmer"):
                 st.spinner("Conversion en cours...")
-                file_path = convert_pdf_to_excel(uploaded_file, supplier)
                 
-                # Appel à la fonction de conversion spécifique au fournisseur
-                if supplier == "Yes Food":
-                    process_yes_food(file_path)
-                elif supplier == "MTC":
-                    process_mtc(file_path)
+                # Convertir le fichier PDF
+                with tempfile.NamedTemporaryFile(delete=False, suffix='.pdf') as temp_pdf:
+                    temp_pdf.write(uploaded_file.read())
+                    temp_pdf_path = temp_pdf.name
+                
+                # Appel à la fonction de conversion et obtenir le fichier Excel
+                file_path = convert_pdf_to_excel(temp_pdf_path, supplier)
+                
+                if file_path:
+                    # Appel à la fonction de traitement spécifique au fournisseur
+                    if supplier == "Yes Food":
+                        process_yes_food(file_path)
+                    elif supplier == "MTC":
+                        process_mtc(file_path)
 
-                st.success("Conversion terminée ! Vous pouvez télécharger le fichier Excel.")
-                with open(file_path, 'rb') as file:
-                    st.download_button("Télécharger le fichier Excel", file, file_name=f"Produits_Prix_{supplier}.xlsx", mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+                    st.success("Conversion terminée ! Vous pouvez télécharger le fichier Excel.")
+                    with open(file_path, 'rb') as file:
+                        st.download_button(
+                            "Télécharger le fichier Excel", 
+                            file, 
+                            file_name=f"Produits_Prix_{supplier}.xlsx", 
+                            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                        )
+                else:
+                    st.error("Erreur lors de la conversion du fichier.")
+                
             if st.button("Annuler"):
                 st.write("La conversion a été annulée.")
         else:
